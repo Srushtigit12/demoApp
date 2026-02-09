df36b31fa5e3aa52441773d4c418db8d16981a87
Srushti Thawkar <srushti.t@sodalessolutions.com>
2026-02-09 10:35:42 +0000

    Latest code

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("demo.controller.View1", {
         onInit() {
+            var worklist = new JSONModel({
+                "sKey": ""
+            });
+            this.oView.setModel(worklist, "worklistView");
         }
     });
 });
